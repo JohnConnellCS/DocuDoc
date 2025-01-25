d65e80e383e4be8a39d969cc64440f6b39a2716f
John Connell <johnconnellcs@gmail.com>
2025-01-25 13:37:31 -0800

    Set up Firebase and Firestore with Post Function

---
+++ src/firebase.js
@@ -0,0 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp  } from "firebase/app";
+import { getFirestore, collection, addDoc, Timestamp } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: "docudoc-e4fd1",
+  storageBucket: "docudoc-e4fd1.firebasestorage.app",
+  messagingSenderId: "722087045855",
+  appId: "1:722087045855:web:6d079dd1319369c486976a",
+  measurementId: "G-CMNW7BNLYF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//  Function to submit conversation data
+async function submitConversationData(
+  rawData,
+  transcriptSummary,
+  postOpSummary
+) {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      transcript: rawData,
+      summary: transcriptSummary,
+      postOperativeSteps: postOpSummary,
+      timestamp: Timestamp.now(),
+    });
+    console.log("Doc written ith ID: ", docRef.id);
+  } catch (e){
+    console.error("Error adding document: ", e);
+  }
+}
+
+
+module.exports = { app, db, submitConversationData};

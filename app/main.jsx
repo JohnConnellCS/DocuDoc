9d9ecbe96fa5b7ec63624751d8b42fc2c2c1ab7d
Yuexin Xu <singledogxu@gmail.com>
2025-01-25 14:07:01 -0800

    button for record

--- app/main.jsx
+++ app/main.jsx
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
 
 const Main = () => {
+  const handleRecord = ()=>{}
   return (
-    <View>
-      <Text>Main</Text>
-    </View>
+    <SafeAreaView className = "bg-white h-full">
+      <ScrollView>
+        <TouchableOpacity onPress={handleRecord} className="bg-primary-200 shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5">
+          <Text>Record</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
